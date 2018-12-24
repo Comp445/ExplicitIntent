8dd0addcd3adcc5c19c68c3b3e6ef56c548b7d6f
Frank Tutino <gyrotoo@gmail.com>
2018-12-23 20:56:56 -0500

    lesson finished

--- app/src/main/java/za/co/programme/jurrius/explicitintent/MainActivity.java
+++ app/src/main/java/za/co/programme/jurrius/explicitintent/MainActivity.java
@@ -1,11 +1,14 @@
 package za.co.programme.jurrius.explicitintent;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     EditText etName;
     Button btnAct2, btnAct3;
     TextView tvResults;
+    final int ACTIVITY3 = 3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,17 +32,51 @@ public class MainActivity extends AppCompatActivity {
         btnAct2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                if(etName.getText().toString().isEmpty())
+                {
+                    Toast.makeText(MainActivity.this, "Please enter name", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    String name = etName.getText().toString().trim();
+                    Intent intent = new Intent(MainActivity.this,za.co.programme.
+                            jurrius.explicitintent.Activity2.class);
+                    intent.putExtra("name", name);
+                    startActivity(intent);
+                }
             }
         });
         btnAct3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                Intent intent = new Intent(MainActivity.this,za.co.programme.
+                        jurrius.explicitintent.Activity3.class);
 
+                startActivityForResult(intent, ACTIVITY3 );
             }
         });
 
 
+
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == ACTIVITY3)
+        {
+            if (resultCode==RESULT_OK)
+            {
+                tvResults.setText(data.getStringExtra("surname"));
+            }
+            if (resultCode==RESULT_CANCELED)
+            {
+                tvResults.setText("no name entered");
+            }
+        }
     }
 }
